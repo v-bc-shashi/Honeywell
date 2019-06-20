50ca7a04591f963c1d25202d2be26194e894fb34
anoj <anoj@anoj-pc>
2019-06-20 18:45:11 +0530

    This is commit to check test controller.

---
+++ HostpitalManagement/src/main/java/com/spp/HostpitalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.spp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostpitalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostpitalManagementApplication.class, args);
+	}
+
+}
